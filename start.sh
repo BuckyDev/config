d95a1527723072fc386a346c86abc7a21dfdde98
BuckyDev <ymeyjonade@gmail.com>
2019-06-02 17:19:34 +0100

    Removed call to reset functions

--- start.sh
+++ start.sh
@@ -9,6 +9,5 @@ printf 'Username :'; read USERNAME; export USERNAME;
 printf 'Mail address :'; read MAIL; export MAIL;
 printf 'Password :'; read PASSWORD; export PASSWORD;
 
-# Launch setup and reset
+# Launch setup 
 cd setup; sh fullstack_setup.sh; cd ..;
-cd reset; sh fullstack_reset.sh; cd ..;
